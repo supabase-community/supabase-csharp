3d2ceb89989e1ed28acac4842f944204e1fc0f11
Joseph Schultz <joseph@acupofjose.com>
2022-09-13 13:00:52 -0500

    Propagate added cancellation token to `SupabaseModel`

--- Supabase/SupabaseModel.cs
+++ Supabase/SupabaseModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Postgrest.Models;
 using Postgrest.Responses;
@@ -7,12 +8,12 @@ namespace Supabase
 {
     public abstract class SupabaseModel : BaseModel
     {
-        public override Task<ModeledResponse<T>> Update<T>()
+        public override Task<ModeledResponse<T>> Update<T>(CancellationToken cancellationToken = default(CancellationToken))
         {
             return Client.Instance.From<T>().Update(this as T);
         }
 
-        public override Task Delete<T>()
+        public override Task Delete<T>(CancellationToken cancellationToken = default(CancellationToken))
         {
             return Client.Instance.From<T>().Delete(this as T);
         }
