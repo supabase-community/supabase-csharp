a0aab1bbdb706ebe727c352da29a6d4d638d5288
Diego Santo <diegofesanto@outlook.com>
2026-09-07 23:16:39 -0300

    feat(gotrue): fix resend param and add to public api doc

--- packages/Gotrue/Gotrue/ResendParam.cs
+++ packages/Gotrue/Gotrue/ResendParam.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Supabase.Gotrue;
@@ -27,27 +26,17 @@ public class ResendParam
     public string? Type { get; set; }
 
     /// <summary>
-    /// Verification token received when the user completes the captcha on the site.
+    /// Additional options to customize the behavior of the Resend API.
+    /// Provides support for features such as CAPTCHA tokens and email redirection.
     /// </summary>
-    [JsonPropertyName("captchaToken")]
-    public string? CaptchaToken { get; set; }
-
-    /// <summary>
-    /// A URL or mobile address to send the user to after they are confirmed.
-    /// </summary>
-    [JsonPropertyName("redirectTo")]
-    public string? RedirectTo { get; set; }
-
-    /// <summary>
-    /// A custom data object to store the user's metadata. This maps to the `auth.users.user_metadata` column.
-    /// </summary>
-    [JsonPropertyName("data")]
-    public Dictionary<string, object>? Data { get; set; }
+    [JsonPropertyName("options")]
+    public ResendOptionsParam? Options { get; set; }
 
     /// <summary>
     /// Represents the parameters used for the Resend API, allowing for resending
     /// confirmation codes or links based on the provided inputs. This includes
     /// support for email, phone, and custom data.
     /// </summary>
-    public ResendParam(Constants.ResendType type) => this.Type = Core.Helpers.GetMappedToAttr(type).Mapping;
+    public ResendParam(Constants.ResendType type) =>
+        this.Type = Core.Helpers.GetMappedToAttr(type).Mapping;
 }
